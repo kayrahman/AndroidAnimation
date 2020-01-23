faa8c00cd21feba223c582de6be88cbac1f6ae6f
kay <k.rahman0511@gmail.com>
2020-01-23 23:05:19 +0600

    rotate,fade,translate,colorize

--- app/src/main/java/com/nkr/androidanimation/MainActivity.kt
+++ app/src/main/java/com/nkr/androidanimation/MainActivity.kt
@@ -1,6 +1,11 @@
 package com.nkr.androidanimation
 
+import android.animation.Animator
+import android.animation.AnimatorListenerAdapter
 import android.animation.ObjectAnimator
+import android.animation.PropertyValuesHolder
+import android.graphics.Color
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
@@ -14,9 +19,111 @@ class MainActivity : AppCompatActivity() {
 
         rotateButton.setOnClickListener {
             val animator = ObjectAnimator.ofFloat(star, View.ROTATION, -360f, 0f)
-          //  animator.duration = 1000
-           // animator.disableViewDuringAnimation(rotateButton)
+            animator.duration = 1000
+
+            animator.disableViewDuringAnimation(rotateButton)
             animator.start()
         }
+
+
+        translateButton.setOnClickListener {
+            translater()
+
+        }
+
+        scaleButton.setOnClickListener {
+            scaler()
+        }
+
+        fadeButton.setOnClickListener {
+
+            fader()
+        }
+
+
+        colorizeButton.setOnClickListener {
+            colorizer()
+        }
+
+
+
     }
+
+    private fun translater() {
+
+        // Translate the view 200 pixels to the right and back
+
+        val animator = ObjectAnimator.ofFloat(star, View.TRANSLATION_X, 200f)
+        animator.repeatCount = 1
+        animator.repeatMode = ObjectAnimator.REVERSE
+        animator.disableViewDuringAnimation(translateButton)
+        animator.start()
+    }
+
+
+    private fun scaler() {
+
+        // Scale the view up to 4x its default size and back
+
+        val scaleX = PropertyValuesHolder.ofFloat(View.SCALE_X, 4f)
+        val scaleY = PropertyValuesHolder.ofFloat(View.SCALE_Y, 4f)
+        val animator = ObjectAnimator.ofPropertyValuesHolder(star, scaleX, scaleY)
+        animator.repeatCount = 1
+        animator.repeatMode = ObjectAnimator.REVERSE
+        animator.disableViewDuringAnimation(scaleButton)
+        animator.start()
+    }
+
+    private fun fader() {
+
+        // Fade the view out to completely transparent and then back to completely opaque
+
+        val animator = ObjectAnimator.ofFloat(star, View.ALPHA, 0f)
+        animator.repeatCount = 1
+        animator.repeatMode = ObjectAnimator.REVERSE
+        animator.disableViewDuringAnimation(fadeButton)
+        animator.start()
+    }
+
+
+
+
+    private fun colorizer() {
+
+        // Animate the color of the star's container from black to red over a half
+        // second, then reverse back to black. Note that using a propertyName of
+        // "backgroundColor" will cause the animator to call the backgroundColor property
+        // (in Kotlin) or setBackgroundColor(int) (in Java).
+
+        var animator = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            ObjectAnimator.ofArgb(star.parent,
+                "backgroundColor", Color.BLACK, Color.CYAN)
+        } else {
+            TODO("VERSION.SDK_INT < LOLLIPOP")
+        }
+        animator.setDuration(500)
+        animator.repeatCount = 1
+        animator.repeatMode = ObjectAnimator.REVERSE
+        animator.disableViewDuringAnimation(colorizeButton)
+        animator.start()
+    }
+
+
+
+    private fun ObjectAnimator.disableViewDuringAnimation(view: View) {
+
+        // This extension method listens for start/end events on an animation and disables
+        // the given view for the entirety of that animation.
+
+        addListener(object : AnimatorListenerAdapter() {
+            override fun onAnimationStart(animation: Animator?) {
+                view.isEnabled = false
+            }
+
+            override fun onAnimationEnd(animation: Animator?) {
+                view.isEnabled = true
+            }
+        })
+    }
+
 }
