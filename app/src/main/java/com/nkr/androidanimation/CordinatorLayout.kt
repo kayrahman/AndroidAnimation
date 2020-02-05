74859153c89bf490a3f463392704e3a9c8c6163f
kay <k.rahman0511@gmail.com>
2020-02-05 19:16:52 +0600

    Coordinator layout

---
+++ app/src/main/java/com/nkr/androidanimation/CordinatorLayout.kt
@@ -0,0 +1,12 @@
+package com.nkr.androidanimation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CordinatorLayout : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_cordinator_layout)
+    }
+}
